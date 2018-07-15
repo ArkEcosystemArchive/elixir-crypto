1b6bcf015cbee8b6bfc6b950530bd2ded2d34735
supaiku <supaiku@protonmail.com>
2018-07-15 15:23:51 +0200

    feat: add TestHelper to read fixtures, new overload for Serialize.serialize

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,14 @@
 ExUnit.start(exclude: [:skip])
+
+defmodule ArkEcosystem.Test.TestHelper do
+
+  @fixtures_path "test/fixtures/transactions/"
+
+  def read_fixture(transaction_type, name) when is_bitstring(transaction_type) do
+    filepath = @fixtures_path <> transaction_type <> "/" <> name <> ".json"
+
+    File.read!(filepath)
+      |> Jason.decode!(%{ :keys => :atoms })
+  end
+
+end
