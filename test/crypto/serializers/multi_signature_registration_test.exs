f4f5eef9e699ca22b4de4b2b5a871402edc032ee
supaiku <supaiku@protonmail.com>
2018-07-15 16:46:32 +0200

    test: use new fixtures for the serializer tests

--- test/crypto/serializers/multi_signature_registration_test.exs
+++ test/crypto/serializers/multi_signature_registration_test.exs
@@ -1,18 +1,21 @@
 defmodule ArkEcosystem.Crypto.Serializers.MultiSignatureRegistrationTest do
   use ExUnit.Case, async: false
   alias ArkEcosystem.Crypto.Serializer
+  alias ArkEcosystem.Test.TestHelper
 
-  test "should be ok" do
-    transaction = File.read!("test/fixtures/transactions/multi_signature_registration.json")
-      |> Jason.decode!(%{ :keys => :atoms })
-      |> ArkEcosystem.Crypto.Utils.Underscorer.underscore
-
+  setup_all do
     ArkEcosystem.Crypto.Configuration.Network.set(
       ArkEcosystem.Crypto.Networks.Devnet
     )
 
-    actual = Serializer.serialize(transaction)
-    assert(actual == transaction.serialized)
+    :ok
+  end
+
+  test "should be ok" do
+    fixture = TestHelper.read_fixture("multi_signature_registration", "passphrase")
+    actual = Serializer.serialize(fixture.data, %{ underscore: true })
+
+    assert(actual == fixture.serialized)
   end
 
 end
