ea654a042ff16c578deae6c7e227c0ab4aa5d717
Brian Faust <hello@brianfaust.me>
2018-07-16 16:56:16 +0300

    refactor: rename secret to passphrase (guide compliance)

--- test/crypto/transactions/builder_test.exs
+++ test/crypto/transactions/builder_test.exs
@@ -13,8 +13,8 @@ defmodule ArkEcosystem.Crypto.Transactions.BuilderTest do
         amount: 133_380_000_000,
         recipient_id: "AXoXnFi4z1Z6aFvjEYkDVCtBGW2PaRiM25",
         vendor_field: "This is a transaction from Elixir",
-        secret: "this is a top secret passphrase",
-        second_secret: "this is a top secret second passphrase",
+        passphrase: "this is a top secret passphrase",
+        second_passphrase: "this is a top secret second passphrase",
         delegate: "034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192"
       ]
     }
@@ -26,26 +26,26 @@ defmodule ArkEcosystem.Crypto.Transactions.BuilderTest do
         context[:recipient_id],
         context[:amount],
         context[:vendor_field],
-        context[:secret]
+        context[:passphrase]
       )
 
     assert(Transaction.verify(transaction) == true)
   end
 
-  test "should build transfer and verfiy with second secret", context do
+  test "should build transfer and verfiy with second passphrase", context do
     transaction =
       Builder.build_transfer(
         context[:recipient_id],
         context[:amount],
         context[:vendor_field],
-        context[:secret],
-        context[:second_secret]
+        context[:passphrase],
+        context[:second_passphrase]
       )
 
-    second_public_key_address = PublicKey.from_passphrase(context[:second_secret])
+    second_public_key = PublicKey.from_passphrase(context[:second_passphrase])
 
     assert(Transaction.verify(transaction) == true)
-    assert(Transaction.second_verify(transaction, second_public_key_address) == true)
+    assert(Transaction.second_verify(transaction, second_public_key) == true)
   end
 
   test "should build vote and verify", context do
@@ -54,62 +54,62 @@ defmodule ArkEcosystem.Crypto.Transactions.BuilderTest do
     transaction =
       Builder.build_vote(
         votes,
-        context[:secret]
+        context[:passphrase]
       )
 
     assert(Transaction.verify(transaction) == true)
   end
 
-  test "should build vote and verify with second secret", context do
+  test "should build vote and verify with second passphrase", context do
     votes = ["+" <> context[:delegate]]
 
     transaction =
       Builder.build_vote(
         votes,
-        context[:secret],
-        context[:second_secret]
+        context[:passphrase],
+        context[:second_passphrase]
       )
 
-    second_public_key_address = PublicKey.from_passphrase(context[:second_secret])
+    second_public_key = PublicKey.from_passphrase(context[:second_passphrase])
 
     assert(Transaction.verify(transaction) == true)
-    assert(Transaction.second_verify(transaction, second_public_key_address) == true)
+    assert(Transaction.second_verify(transaction, second_public_key) == true)
   end
 
   test "should build second signature registration and verify", context do
     transaction =
       Builder.build_second_signature_registration(
-        context[:secret],
-        context[:second_secret]
+        context[:passphrase],
+        context[:second_passphrase]
       )
 
-    second_public_key_address = PublicKey.from_passphrase(context[:second_secret])
+    second_public_key = PublicKey.from_passphrase(context[:second_passphrase])
 
     assert(Transaction.verify(transaction) == true)
-    assert(Transaction.second_verify(transaction, second_public_key_address) == true)
+    assert(Transaction.second_verify(transaction, second_public_key) == true)
   end
 
   test "should build delegate registration and verify", context do
     transaction =
       Builder.build_delegate_registration(
         "Moo",
-        context[:secret]
+        context[:passphrase]
       )
 
     assert(Transaction.verify(transaction) == true)
   end
 
-  test "should build delegate registration and verify with second secret", context do
+  test "should build delegate registration and verify with second passphrase", context do
     transaction =
       Builder.build_delegate_registration(
         "Moo",
-        context[:secret],
-        context[:second_secret]
+        context[:passphrase],
+        context[:second_passphrase]
       )
 
-    second_public_key_address = PublicKey.from_passphrase(context[:second_secret])
+    second_public_key = PublicKey.from_passphrase(context[:second_passphrase])
 
     assert(Transaction.verify(transaction) == true)
-    assert(Transaction.second_verify(transaction, second_public_key_address) == true)
+    assert(Transaction.second_verify(transaction, second_public_key) == true)
   end
 end
