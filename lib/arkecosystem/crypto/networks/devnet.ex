4d33e8e8ea9d4a226193e3cd9ec627312a2a9929
Brian Faust <hello@brianfaust.me>
2018-07-17 10:16:35 +0300

    refactor: remove nethash (not used)

--- lib/arkecosystem/crypto/networks/devnet.ex
+++ lib/arkecosystem/crypto/networks/devnet.ex
@@ -2,7 +2,6 @@ defmodule ArkEcosystem.Crypto.Networks.Devnet do
   defstruct(
     epoch: "2017-03-21T13:00:00.000Z",
     version: "1e",
-    nethash: "578e820911f24e039733b45e4882b73e301f813a0d2c31330dafda84534ffa23",
     wif: 170
   )
 end
