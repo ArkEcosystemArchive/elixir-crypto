e8775a1fbb14055888201faa48d286072293df66
Brian Faust <envoyer@pm.me>
2018-07-31 08:45:09 +0300

    chore: 0.1.1 changelog
    Signed-off-by: Brian Faust <envoyer@pm.me>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,5 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## Unreleased
 
+## 0.1.1 - 2018-07-31
+
+### Fixed
+- Properly calculate the transaction `id` if signed with second signature.
+
 ## 0.1.0 - 2018-07-18
 - Initial Release
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArkEcosystem.Crypto.MixProject do
   def project do
     [
       app: :arkecosystem_crypto,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
